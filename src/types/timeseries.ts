897675e46118440ccfa12bfcbb30b83c0b1a333c
Apurva Shah <shahh.apurva@gmail.com>
2023-08-13 02:16:27 -0700

    Added eslint preferences and script actions.

--- src/types/timeseries.ts
+++ src/types/timeseries.ts
@@ -1,8 +1,8 @@
 export interface TimeSeries {
-    t: number[];
-    power: number[];
-    vo2: number[];
-    vco2: number[];
-    ve: number[];
-    hr: number[];
+  t: number[];
+  power: number[];
+  vo2: number[];
+  vco2: number[];
+  ve: number[];
+  hr: number[];
 }
