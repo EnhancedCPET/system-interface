861dde0adc569b4bdf5f1e6b3dd38014fc50fabe
Apurva Shah <shahh.apurva@gmail.com>
2023-08-13 01:38:11 -0700

    Added interfaces for extracted timeseries and demographics
    Added the fundamental interfaces for the data that needs to
    be extracted from the excel file.

---
+++ src/types/demographics.ts
@@ -0,0 +1,11 @@
+export interface Demographics {
+    id?: string;
+    fname?: string;
+    lname?: string;
+    age: number;
+    dob?: string;
+    height: number;
+    weight: number;
+    gender: string;
+    race?: string;
+}
---
+++ src/types/timeseries.ts
@@ -0,0 +1,8 @@
+export interface TimeSeries {
+    t: number[];
+    power: number[];
+    vo2: number[];
+    vco2: number[];
+    ve: number[];
+    hr: number[];
+}
