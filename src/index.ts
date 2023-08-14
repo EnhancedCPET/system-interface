fa6bc38389d989e284dfcbf0c404a19fed8442b8
Apurva Shah <shahh.apurva@gmail.com>
2023-08-13 17:12:55 -0700

    refactor(api): refactored api interface to remove repeated extraction

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,31 @@
 import { processCosmed } from './systems/cosmed';
 import * as XLSX from 'xlsx';
 
+function extractDataFromSheet(sheet: XLSX.WorkSheet, system: string) {
+    switch (system.toLowerCase()) {
+        case 'cosmed':
+            return processCosmed(sheet);
+        default:
+            throw new Error('Unsupported system');
+    }
+}
+
 const SystemInterface = {
-  extractFileData(filePath: string, system: string) {
-    const workbook = XLSX.readFile(filePath);
-    const sheet_name_list = workbook.SheetNames;
-    const sheet = workbook.Sheets[sheet_name_list[0]];
+    extractFileData(filePath: string, system: string) {
+        const workbook = XLSX.readFile(filePath);
+        const sheet_name_list = workbook.SheetNames;
+        const sheet = workbook.Sheets[sheet_name_list[0]];
 
-    switch (system.toLowerCase()) {
-      case 'cosmed':
-        return processCosmed(sheet);
-      default:
-        throw new Error('Unsupported system');
-    }
-  },
+        return extractDataFromSheet(sheet, system);
+    },
 
-  extractBufferData(bufferArray: ArrayBuffer, system: string) {
-    const workbook = XLSX.read(bufferArray, { type: 'buffer' });
-    const sheet_name_list = workbook.SheetNames;
-    const sheet = workbook.Sheets[sheet_name_list[0]];
+    extractBufferData(bufferArray: ArrayBuffer, system: string) {
+        const workbook = XLSX.read(bufferArray, { type: 'buffer' });
+        const sheet_name_list = workbook.SheetNames;
+        const sheet = workbook.Sheets[sheet_name_list[0]];
 
-    switch (system.toLowerCase()) {
-      case 'cosmed':
-        return processCosmed(sheet);
-      default:
-        throw new Error('Unsupported system');
-    }
-  },
+        return extractDataFromSheet(sheet, system);
+    },
 };
 
 export { SystemInterface };
